4383ee195c50f8c31c927673317ddd7c5b31191d
David <davidcking.inbox@gmail.com>
2023-05-08 12:06:22 -0600

    retained id_evansChromatinStates for later foreign key

--- R/getEvansChromatinStatesAsGRanges.R
+++ R/getEvansChromatinStatesAsGRanges.R
@@ -7,7 +7,7 @@ source('DB_connect.R')
 
 # load saved table if available
 evansChromatinStates.df = dbReadTableCached("evansChromatinStates", "NishimuraLab")
-evansChromatinStates.df %<>% select(-id_evansChromatinStates, update_time)
+evansChromatinStates.df %<>% select(-update_time)
 head(evansChromatinStates.df)  
 evansChromatinStates.gr = GRanges(evansChromatinStates.df)
 head(evansChromatinStates.gr)
